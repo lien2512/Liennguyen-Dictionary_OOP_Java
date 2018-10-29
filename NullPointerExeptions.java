630289f8b124cebb0c5b06f6265b9cb6d5040d0b
lien2512 <43362844+lien2512@users.noreply.github.com>
2018-10-29 10:05:37 +0700

    Add files via upload

---
+++ NullPointerExeptions.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NullPointerExeptions {
+
+    public static void print(String s) throws NullPointerException
+    {
+        if ( s.length() <  10) {
+            System.err.println("đã xảy ra ngoại lệ NullPointerException");
+        } else {
+            System.out.println(s.subSequence(10,12));
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String s = "adjasfa";
+        print(s);
+
+    }
+}
